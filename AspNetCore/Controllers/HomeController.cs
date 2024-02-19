7823cf13a11b3c188e848f3934438e7ea166b056
SirinovRamazan <ramazanvsh@code.edu.az>
2024-02-19 23:35:03 +0400

    done

---
+++ AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Controllers
+{
+    public  class HomeController : Controller
+    {
+        public async  Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
